713f149a4515f0ccfc1b33c7d509fa034e1e46f3
ryan.ramage <ryan.ramage@60a8f656-3bca-11de-90e1-f9f1724fc9cc>
2009-12-08 10:14:52 +0000

    Major refactoring. Cleaner code, simpler external API, Integration tests now show both modes.

--- trunk/springbindwebdriver/src/test/java/com/googlecode/greenbridge/binder/webdriver/IntegrationTest.java
+++ trunk/springbindwebdriver/src/test/java/com/googlecode/greenbridge/binder/webdriver/IntegrationTest.java
@@ -5,12 +5,12 @@
 
 package com.googlecode.greenbridge.binder.webdriver;
 
-import com.googlecode.greenbridge.binder.webdriver.ListCollectionListener;
+import com.googlecode.greenbridge.binder.webdriver.PathListener;
 import com.googlecode.greenbridge.web.FormBean;
 import com.googlecode.greenbridge.web.SubBean;
 import com.googlecode.greenbridge.binder.webdriver.Binder.INPUT_TYPE;
 import com.thoughtworks.xstream.XStream;
-import java.io.File;
+import java.io.File; 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.text.ParseException;
@@ -21,9 +21,12 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Speed;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.propertyeditors.CustomBooleanEditor;
@@ -39,13 +42,19 @@ public class IntegrationTest {
     private static String base = "http://localhost:8181/test/form.html";
     private static String output_dir_txt = "target/binding";
     private static File output_dir;
+    private SimpleDateFormat dateFormat;
 
     public IntegrationTest() {
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        dateFormat.setLenient(false);
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-        wd = new HtmlUnitDriver(true);
+        //wd = new HtmlUnitDriver(true);
+        //wd = new InternetExplorerDriver();
+        wd = new FirefoxDriver();
+        //wd.manage().setSpeed(Speed.FAST);
         output_dir = new File(output_dir_txt);
         output_dir.mkdirs();
     }
@@ -56,104 +65,108 @@ public class IntegrationTest {
     }
 
     @Test
-    @Ignore
-    public void doTest() throws FileNotFoundException, ParseException {
+    public void testBindingIndividualProperties() throws FileNotFoundException, ParseException {
         String testId = "test1";
 
         wd.get(base);
         wd.findElement(By.name("testId")).sendKeys(testId);
+
         Binder binder = new Binder();
-        FormBean bean = new FormBean();
-        BeanWrapperImpl wrap = new BeanWrapperImpl(bean);
         CustomBooleanEditor boolEdit = new CustomBooleanEditor("Y", "N", true);
-        wrap.registerCustomEditor(FormBean.class, "inputText2", boolEdit);
-        wrap.registerCustomEditor(FormBean.class, "radio2", boolEdit);
-        wrap.registerCustomEditor(FormBean.class, "select2", boolEdit);
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        dateFormat.setLenient(false);
+        binder.addPropertyEditor(Boolean.class, boolEdit);
         CustomDateEditor dateEditor = new CustomDateEditor(dateFormat, true);
-        wrap.registerCustomEditor(FormBean.class, "inputText4", dateEditor);
-        wrap.registerCustomEditor(FormBean.class, "radio4", dateEditor);
-        wrap.registerCustomEditor(FormBean.class, "select4", dateEditor);
+        binder.addPropertyEditor(Date.class, dateEditor);
 
+        FormBean bean = createFormBean();
 
+        binder.bind( bean,"inputText", wd)
+                .bind("checkbox1")
+                .bind("radio1")
+                .bind("select1")
+                .bind("inputText2")
+                .bind("radio2")
+                .bind("checkbox2")
+                .bind("select2")
+                .bind("inputText3")
+                .bind("radio3")
+                .bind("select3")
+                .bind("inputText4")
+                .bind("radio4")
+                .bind("select4")
+                .bind("subbean.prop1")
+                .bind("subbean.date1");
 
-        bean.setInputText("A test");
-        INPUT_TYPE inputText_type = binder.determineInputType("inputText", wd);
-        assertEquals(INPUT_TYPE.inputText, inputText_type);
-        binder.bindProperty("inputText", wrap, wd);
-
-        bean.setCheckbox1("b");
-        INPUT_TYPE checkbox_type = binder.determineInputType("checkbox1", wd);
-        assertEquals(INPUT_TYPE.checkbox, checkbox_type);
-        binder.bindProperty("checkbox1", wrap, wd);
-
-
-
-        bean.setRadio1("c");
-        INPUT_TYPE radio_type = binder.determineInputType("radio1", wd);
-        assertEquals(INPUT_TYPE.radio, radio_type);
-        binder.bindProperty("radio1", wrap, wd);
-
-
-        bean.setSelect1("c");
-        INPUT_TYPE select_type = binder.determineInputType("select1", wd);
-        assertEquals(INPUT_TYPE.select, select_type);        
-        binder.bindProperty("select1", wrap, wd);
-
-
-
-        bean.setInputText2(Boolean.TRUE);
-        INPUT_TYPE input2_type = binder.determineInputType("inputText2", wd);
-        assertEquals(INPUT_TYPE.inputText, input2_type);
-        binder.bindProperty("inputText2", wrap, wd);
-
-        bean.setRadio2(Boolean.TRUE);
-        INPUT_TYPE radio2_type = binder.determineInputType("radio2", wd);
-        assertEquals(INPUT_TYPE.radio, radio2_type);
-        binder.bindProperty("radio2", wrap, wd);
-
-        bean.setCheckbox2(Boolean.TRUE);
-        binder.bindProperty("checkbox2", wrap, wd);
-
-
-        bean.setSelect2(Boolean.FALSE);
-        INPUT_TYPE select2_type = binder.determineInputType("select2", wd);
-        assertEquals(INPUT_TYPE.select, select2_type);
-        binder.bindProperty("select2", wrap, wd);
-
-
-        bean.setInputText3(5);
-        binder.bindProperty("inputText3", wrap, wd);
-
-        bean.setRadio3(2);
-        binder.bindProperty("radio3", wrap, wd);
-
-        bean.setSelect3(3);
-        binder.bindProperty("select3", wrap, wd);
-
-        bean.setInputText4(dateFormat.parse("02/02/2001"));
-        binder.bindProperty("inputText4", wrap, wd);
-
-        bean.setRadio4(dateFormat.parse("31/12/2009"));
-        binder.bindProperty("radio4", wrap, wd);
-
-        bean.setSelect4(dateFormat.parse("02/12/2009"));
-        binder.bindProperty("select4", wrap, wd);
-
-        bean.getSubbean().setProp1("Sub 1");
-        BeanWrapperImpl subbean = new BeanWrapperImpl(bean.getSubbean(), "subbean", bean);
-        binder.bindProperty("prop1", subbean, wd);
-
-        subbean.registerCustomEditor(SubBean.class, "date1", dateEditor);
-        bean.getSubbean().setDate1(dateFormat.parse("01/01/2007"));
-        binder.bindProperty("date1", subbean, wd);
-
+        wd.findElement(By.id("addRow")).click();
+        binder.bind("list[0].prop1");
 
         wd.findElement(By.id("next")).click();
         assertEquals("Done", wd.findElement(By.id("done")).getText());
 
+        verifyBinding(testId, bean);
+
+    }
+
+
+
+    @Test
+    public void bindBean() throws FileNotFoundException, ParseException {
+        String testId = "bindBean";
+        wd.get(base);
+        wd.findElement(By.name("testId")).sendKeys(testId);
+
+
+        Binder binder = new Binder();
+        binder.addPathListener("subbean\\.prop1", new AbstractPathListener() {
+            @Override
+            public void afterPathDataBind(String path, Object pathValue, WebDriver wd) {
+                wd.findElement(By.id("addRow")).click();
+            }
+        });
+
+        binder.addPathListener("list\\[0\\]\\.prop1", new AbstractPathListener() {
+            @Override
+            public void beforePathDataBind(String path, Object pathValue, WebDriver wd) {
+                wd.findElement(By.id("addRow")).click();
+            }
+        });
+
+        CustomBooleanEditor boolEdit = new CustomBooleanEditor("Y", "N", true);
+        binder.addPropertyEditor(Boolean.class, boolEdit);
+        CustomDateEditor dateEditor = new CustomDateEditor(dateFormat, true);
+        binder.addPropertyEditor(Date.class, dateEditor);
+
+
+        FormBean bean = createFormBean();
+        binder.bindOrdered(bean, "inputText", wd);
+
+        wd.findElement(By.id("next")).click();
+        assertEquals("Done", wd.findElement(By.id("done")).getText());
+
+        verifyBinding(testId, bean);
+    }
+
+    protected FormBean createFormBean() throws ParseException {
+        FormBean bean = new FormBean();
+        bean.setInputText("A test");
+        bean.setCheckbox1("b");
+        bean.setRadio1("c");
+        bean.setSelect1("c");
+        bean.setInputText2(Boolean.TRUE);
+        bean.setRadio2(Boolean.TRUE);
+        bean.setSelect2(Boolean.FALSE);
+        bean.setInputText3(5);
+        bean.setRadio3(2);
+        bean.setSelect3(3);
+        bean.setInputText4(dateFormat.parse("02/02/2001"));
+        bean.setRadio4(dateFormat.parse("31/12/2009"));
+        bean.setSelect4(dateFormat.parse("02/12/2009"));
+        bean.getSubbean().setProp1("Sub 1");
+        bean.getSubbean().setDate1(dateFormat.parse("01/01/2007"));
+        bean.getList().get(0).setProp1("A1");
+        return bean;
+    }
+
+    protected void verifyBinding(String testId, FormBean bean) throws FileNotFoundException {
         XStream xstream = new XStream();
         File resultFile = new File(output_dir, testId + ".xml");
         FileReader reader = new FileReader(resultFile);
@@ -164,7 +177,6 @@ public class IntegrationTest {
         assertEquals(bean.getSelect1(), result.getSelect1());
         assertEquals(bean.getInputText2(), result.getInputText2());
         assertEquals(bean.getRadio2(), result.getRadio2());
-        assertEquals(bean.getCheckbox2(), result.getCheckbox2());
         assertEquals(bean.getSelect2(), result.getSelect2());
         assertEquals(bean.getInputText3(), result.getInputText3());
         assertEquals(bean.getRadio3(), result.getRadio3());
@@ -174,111 +186,7 @@ public class IntegrationTest {
         assertEquals(bean.getSelect4(), result.getSelect4());
         assertEquals(bean.getSubbean().getProp1(), result.getSubbean().getProp1());
         assertEquals(bean.getSubbean().getDate1(), result.getSubbean().getDate1());
-    }
-
-
-
-    @Test
-    public void bindBean() throws FileNotFoundException, ParseException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-
-        String testId = "bindBean";
-        wd.get(base);
-        wd.findElement(By.name("testId")).sendKeys(testId);
-        Binder binder = new Binder();
-        FormBean bean = new FormBean();
-        bean.setInputText("Cool, this is!");
-        bean.setCheckbox1("b");
-        bean.setRadio1("b");
-        bean.setSelect1("c");
-
-        bean.setInputText2(Boolean.TRUE);
-        bean.setRadio2(Boolean.TRUE);
-        bean.setSelect2(Boolean.FALSE);
-
-        bean.setInputText3(5);
-        bean.setRadio3(2);
-        bean.setSelect3(3);
-
-        bean.setInputText4(dateFormat.parse("02/02/2001"));
-        bean.setRadio4(dateFormat.parse("31/12/2009"));
-        bean.setSelect4(dateFormat.parse("02/12/2009"));
-
-
-        bean.getSubbean().setProp1("Super D Duper");
-        bean.getSubbean().setDate1(dateFormat.parse("01/01/2007"));
-
-        bean.getList().get(0).setProp1("A1");
-        binder.addListCollectionListener("list.*", new ListCollectionListener() {
-            @Override
-            public void beforeListCollectionEntry(int index, Object element, WebDriver wd) {
-                wd.findElement(By.id("addRow")).click();
-            }
-        });
-
-        CustomBooleanEditor boolEdit = new CustomBooleanEditor("Y", "N", true);
-        binder.addPropertyEditor(Boolean.class, boolEdit);
-        
-        dateFormat.setLenient(false);
-        CustomDateEditor dateEditor = new CustomDateEditor(dateFormat, true);
-        binder.addPropertyEditor(Date.class, dateEditor);
-
-
-
-        binder.bindBean(bean, wd);
-
-        wd.findElement(By.id("next")).click();
-        assertEquals("Done", wd.findElement(By.id("done")).getText());
-
-        XStream xstream = new XStream();
-        File resultFile = new File(output_dir, testId + ".xml");
-        FileReader reader = new FileReader(resultFile);
-        FormBean result = (FormBean)xstream.fromXML(reader);
-        assertEquals(bean.getInputText(), result.getInputText());
-        assertEquals(bean.getCheckbox1(), result.getCheckbox1());
-        assertEquals(bean.getRadio1(), result.getRadio1());
-        assertEquals(bean.getSelect1(), result.getSelect1());
-        assertEquals(bean.getInputText2(), result.getInputText2());
-        assertEquals(bean.getRadio2(), result.getRadio2());
-        assertEquals(bean.getSelect2(), result.getSelect2());
-        assertEquals(bean.getInputText3(), result.getInputText3());
-        assertEquals(bean.getRadio3(), result.getRadio3());
-        assertEquals(bean.getSelect3(), result.getSelect3());
-
-        assertEquals(bean.getSubbean().getProp1(), result.getSubbean().getProp1());
-        assertEquals(bean.getSubbean().getDate1(), result.getSubbean().getDate1());
         assertEquals(bean.getList().get(0).getProp1(), result.getList().get(0).getProp1());
-    }
-
-
-    @Ignore
-    @Test
-    public void testNulls() throws FileNotFoundException {
-        String testId = "nullTestBinding";
-        wd.get(base);
-        wd.findElement(By.name("testId")).sendKeys(testId);
-        Binder binder = new Binder();
-        FormBean bean = new FormBean();
-        bean.setInputText(null);
-        bean.setCheckbox1(null);
-        bean.setRadio1(null);
-        bean.setSelect1(null);
-        BeanWrapperImpl wrap = new BeanWrapperImpl(bean);
-
-
-        binder.bindBean(wrap, wd);
-
-        wd.findElement(By.id("next")).click();
-        assertEquals("Done", wd.findElement(By.id("done")).getText());
-
-        XStream xstream = new XStream();
-        File resultFile = new File(output_dir, testId + ".xml");
-        FileReader reader = new FileReader(resultFile);
-        FormBean result = (FormBean)xstream.fromXML(reader);
-        assertEquals("", result.getInputText());
-        assertEquals(bean.getCheckbox1(), result.getCheckbox1());
-        assertEquals(bean.getRadio1(), result.getRadio1());
-        assertEquals("a", result.getSelect1());
     }
 
 }
