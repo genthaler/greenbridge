c8bea97cf582d3d6dea265cb6aaed12c355beea8
ryan.ramage <ryan.ramage@60a8f656-3bca-11de-90e1-f9f1724fc9cc>
2009-12-21 10:16:08 +0000

    Initial commit of Greenbridge conversations as a couchdb app.

---
+++ couch/greenbridge/scripts/navigation.js
@@ -0,0 +1,33 @@
+$(function() {
+	 $.get("_header.html", function(resp) {
+		$("#header").append(resp);
+		$('#tagsearch').autocomplete('/conversations/_design/tags/_view/by_tag_grouped', {
+			multiple: false,
+			minChars: 1,
+			max: 300,
+			dataType: "json",
+			extraParams: {
+				startkey: function() {return '"' + $('#tagsearch').val() + '"'},
+				group: 'true'
+			},
+			parse: function(data) {
+				var arr = new Array();
+				for (var i=0; i < data.rows.length;i++) {
+					arr[i] = {
+						data: data.rows[i],
+						value: data.rows[i].key,
+						result: data.rows[i].key
+					};
+				}
+				return arr;
+			},
+			formatItem: function(item) {
+				return item.key + " (" + item.value + ")" ;
+			}
+
+		});
+	 });
+	 $('#content').corner();
+	 
+	 
+});
