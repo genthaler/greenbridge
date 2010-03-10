c8fa2fc57bb90fd439e669a4ec28754d46bc1870
ryan.ramage <ryan.ramage@60a8f656-3bca-11de-90e1-f9f1724fc9cc>
2010-03-10 10:25:02 +0000

    Showing off the new Parameterized test, and the better way to add annotations so less to change on a version increase

---
+++ greenbridge-example-archetype/src/main/resources/archetype-resources/src/test/java/ParameterizedTableTest.java
@@ -0,0 +1,53 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+
+import com.googlecode.greenbridge.annotation.RunWithDelegate;
+import com.googlecode.greenbridge.annotation.Scenario;
+import com.googlecode.greenbridge.annotation.Story;
+import com.googlecode.greenbridge.junit.GreenbridgeRunner;
+import java.math.BigDecimal;
+import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import static ${packageQualifier}.scenarios.TestTable.*;
+/**
+ *
+ * @author ryan
+ */
+@RunWith(GreenbridgeRunner.class)
+@RunWithDelegate(Parameterized.class)
+@Story(${packageQualifier}.stories.SampleStory1_2.class)
+public class ParameterizedTableTest {
+    private BigDecimal prevBalance;
+    private BigDecimal deposit;
+    private BigDecimal newBalance;
+
+
+    @Parameters
+    public static Collection generateParameters()  {
+        return DATA_TABLE.generateParameters();
+    }
+
+    public ParameterizedTableTest(String prevBalance, String deposit, String newBalance) {
+        this.prevBalance = new BigDecimal(prevBalance);
+        this.deposit = new BigDecimal(deposit);
+        this.newBalance = new BigDecimal(newBalance);
+    }
+
+    @Test
+    @Scenario(${packageQualifier}.scenarios.TestTable_4.class)
+    public void ensureAdeposit(){
+        BigDecimal newAmount = prevBalance.add(deposit);
+        System.out.println("New Balance: " + newAmount);
+        System.out.println("shouldBe: " + newBalance);
+        Assert.assertTrue(prevBalance.add(deposit).compareTo(newBalance) == 0);
+    }
+
+}
