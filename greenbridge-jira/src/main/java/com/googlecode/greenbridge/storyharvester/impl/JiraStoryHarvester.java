2cc4e42917e63c85cc39a0ead3baa2a196e853c5
ryan.ramage <ryan.ramage@60a8f656-3bca-11de-90e1-f9f1724fc9cc>
2010-03-24 22:00:59 +0000

    Ability to load a local file for a story.

--- greenbridge-jira/src/main/java/com/googlecode/greenbridge/storyharvester/impl/JiraStoryHarvester.java
+++ greenbridge-jira/src/main/java/com/googlecode/greenbridge/storyharvester/impl/JiraStoryHarvester.java
@@ -67,25 +67,10 @@ public class JiraStoryHarvester implements StoryHarvester {
 
     @Override
     public List<StoryNarrative> gather() throws Exception {
-        URL url = getSourceURL();
+        
 
-        HttpClient httpClient = new DefaultHttpClient();
-        List<NameValuePair> formparams = new ArrayList<NameValuePair>();
-        formparams.add(new BasicNameValuePair("os_username", jiraUser));
-        formparams.add(new BasicNameValuePair("os_password", jiraPassword));
-        formparams.add(new BasicNameValuePair("os_cookie", "true"));
-        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, "UTF-8");
-        HttpPost post = new HttpPost(getJiraRootUrl() + "/secure/login.jsp");
-        post.setEntity(entity);
-        HttpResponse response = httpClient.execute(post);
-        response.getEntity().consumeContent();
 
-        String url_str = StringEscapeUtils.unescapeXml(jiraFilterURL);
-        HttpGet get = new HttpGet(url_str);
-        response =  httpClient.execute(get);
-
-        Document d = parseDocument(response.getEntity().getContent());
-
+        Document d = loadDocument();
 
         List<Element> nodes = findAllNodes(d);
         List<Element> xmlstories = filterStories(nodes);
@@ -97,6 +82,31 @@ public class JiraStoryHarvester implements StoryHarvester {
         return new ArrayList<StoryNarrative>(stories.values());
     }
 
+
+    protected Document loadDocument() throws MalformedURLException, DocumentException, IOException {
+        if (jiraFilterURL.startsWith("file")) {
+            URL url = getSourceURL();
+            return parseDocument(url);
+        }
+        else {
+            HttpClient httpClient = new DefaultHttpClient();
+            List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+            formparams.add(new BasicNameValuePair("os_username", jiraUser));
+            formparams.add(new BasicNameValuePair("os_password", jiraPassword));
+            formparams.add(new BasicNameValuePair("os_cookie", "true"));
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, "UTF-8");
+            HttpPost post = new HttpPost(getJiraRootUrl() + "/secure/login.jsp");
+            post.setEntity(entity);
+            HttpResponse response = httpClient.execute(post);
+            response.getEntity().consumeContent();
+
+            String url_str = StringEscapeUtils.unescapeXml(jiraFilterURL);
+            HttpGet get = new HttpGet(url_str);
+            response =  httpClient.execute(get);
+
+            return parseDocument(response.getEntity().getContent());
+        }
+    }
 
     protected Document parseDocument(InputStream stream) throws DocumentException {
         SAXReader reader = new SAXReader();
